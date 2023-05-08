9a733d6a90abc5f7ba8d61a1945c29e5889fe23d
Ella Wileman <ecw70574@uga.edu>
2023-05-08 12:53:46 -0400

    added files

---
+++ src/main/java/cs1302/api/Sentence.java
@@ -0,0 +1,12 @@
+package cs1302.api;
+
+/**
+ * Each sentence object taken from an array in Request3Response has an
+ * individual score tag and text.
+ */
+public class Sentence {
+    String score_tag;
+    String inip;
+    String endp;
+    String text;
+} // Sentence
